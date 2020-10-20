08c5cc299bd17927a492e26d5a7e8cef7569e983
neha <44253440+Neha090@users.noreply.github.com>
2020-10-20 17:24:04 +0530

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -7,35 +7,36 @@ public class Question3 {
 	{
 		try 
 		{
-			Log my_log= new Log("log_ques3.txt");
-			my_log.logger.setLevel(Level.INFO);
+			Log myLog= new Log("log_ques3.txt");
+			myLog.logger.setLevel(Level.INFO);
 			
 	   int arr[]= {1,2,3,4,4,5,5,6,7,8,9,10,11,12,13,14,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
 
-	   HashMap<Integer,Integer> Hmap=new HashMap<>();
+	   HashMap<Integer,Integer> hMap=new HashMap<>();
 	   
        for(int i=0;i<100;i++)
        {
-    	   if(Hmap.containsKey(arr[i]))
+    	   if(hMap.containsKey(arr[i]))
     	   {
-    		   Hmap.put(arr[i],Hmap.get(arr[i])+1);
+    		   hMap.put(arr[i],hMap.get(arr[i])+1);
     	   }
     	   else
-    		   Hmap.put(arr[i],1);
+    		   hMap.put(arr[i],1);
        }
        System.out.println("The numbers repeating more than 1 time : ");
-       for (Map.Entry<Integer, Integer> iter : Hmap.entrySet())
+       
+      for (Map.Entry<Integer, Integer> iter : hMap.entrySet())
        {
          if((int)iter.getValue()>1) 
          {
-        	 my_log.logger.info(iter.getKey() +" ");
-        	// System.out.print(iter.getKey() +" ");
+        	 myLog.logger.info(iter.getKey() +" ");
+        	
          }
        }
 		}
 		catch(Exception e)
 		{
-			
+			System.out.print(e);
 		}
        
 
