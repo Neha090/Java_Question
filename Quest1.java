5120914e6f76bc5d1f98aa761f6ec873b5ee3b29
neha <44253440+Neha090@users.noreply.github.com>
2020-10-20 17:20:13 +0530

    Update Quest1.java

--- Quest1.java
+++ Quest1.java
@@ -9,32 +9,33 @@ public class Quest1 {
 	{
 		try 
 		{
-			Log my_log= new Log("log_ques1.txt");
-			my_log.logger.setLevel(Level.INFO);
+			Log myLog= new Log("log_ques1.txt");
+			myLog.logger.setLevel(Level.INFO);
 		
 		int arr[]= {1,2,3,5,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100};
 		
-		TreeSet<Integer> Tset=new TreeSet<>();
+		TreeSet<Integer> tSet=new TreeSet<>();
 		for(int i=0;i<arr.length;i++)
 		{
-			Tset.add(arr[i]);
+			tSet.add(arr[i]);
 		}
 		
-		Iterator<Integer> itr = Tset.iterator(); 
+		Iterator<Integer> itr = tSet.iterator(); 
       
-		int first_num=itr.next();
+		int firstNum=itr.next();
        
 		while (itr.hasNext()) 
         {
 			
-        	int second_num=itr.next(); 
-        	if(first_num-second_num==-2)
+        	int secondNum=itr.next(); 
+        	
+		if(firstNum-secondNum==-2)
         	{
-        		my_log.logger.info("The number missing is : "+(first_num+1));
-        		System.out.println("The number missing is : "+(first_num+1));
+        		myLog.logger.info("The number missing is : "+(firstNum+1));
+        		System.out.println("The number missing is : "+(firstNum+1));
         		break;
         	}
-        	first_num=second_num;
+        	firstNum=seconsNum;
         	
             
         }
@@ -42,7 +43,7 @@ public class Quest1 {
 		}
 		catch(Exception e)
 		{
-			
+			Systemout.println(e);	
 		}
 	}
 }
